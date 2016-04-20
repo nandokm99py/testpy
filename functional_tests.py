a8f2bdbe3d38a9e4a21a169f22a79789f0a45a4f
nandokm <nandokm@gmail.com>
2016-04-20 14:55:54 -0300

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -32,30 +32,13 @@ class NewVisitorTest(unittest.TestCase):
 
         #Verifica no título e no cabeçalho que é uma lista de coisas a fazer
         #assert 'to-do' in browser.title, "Browser title was " + browser.title
-        #self.assertIn('To-Do', self.browser.title)
-        self.assertIn('Django', self.browser.title)
-        header_text= self.browser.find_element_by_tag_name('h1').text
-        #self.assertIn('To-Do', header_text)
-        self.assertIn('worked', header_text)
+        self.assertIn('To-Do', self.browser.title)
+        #self.assertIn('Django', self.browser.title)
+       # header_text= self.browser.find_element_by_tag_name('h1').text
+       # self.assertIn('To-Do', header_text)
+        #self.assertIn('worked', header_text)
 
-        #É convidado a inserir imediatamente uma coisa a fazer
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
 
-        #Digita "Comprar parafusos" numa caixa de texto
-        inputbox.send_keys('Comprar parafusos')
-
-        #Após teclar ENTER a página é atualizada e lista ...
-        inputbox.send_keys(Keys.ENTER)
-
-        table =  self.browser.find_element_by_id('id_list_tabe')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Comprar parafusos' for row in rows)
-        )
 
         #Ainda há uma caixa de texto esperando para adicionar outro item. Ele
         #digita "Verificar se a quantidade de parafusos é suficiente"
