557a42d8a3e0483bca751f0de1a07f96c4b0495b
ubuntu-py <you@example.com>
2016-04-21 05:22:09 -0700

    Troquei parafusos por feathers

--- functional_tests.py
+++ functional_tests.py
@@ -23,6 +23,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
     
         #browser = webdriver.Firefox()
@@ -46,19 +51,13 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
-        #Digita "Comprar parafusos" numa caixa de texto
-        inputbox.send_keys('Comprar parafusos')
+        #Digita "Buy peacock feathers" numa caixa de texto
+        inputbox.send_keys('Buy peacock feathers')
 
-        #Após teclar ENTER a página é atualizada e lista ...
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-
-        #import time
-        #time.sleep(10)
-
-        table =  self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Comprar parafusos', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -68,18 +67,11 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        table =  self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.assertIn('1: Comprar parafusos', [row.text for row in rows])
+        # Edith wonders whether the site will remember her list. Then she sees
 
-        #table = self.browser.find_element_by_id('id_list_table')
-        #rows = table.find_elements_by_tag_name('tr')
-        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly' ,
-             [row.text for row in rows]
-        )
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
@@ -87,13 +79,6 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
-
-
-        #Ainda há uma caixa de texto esperando para adicionar outro item. Ele
-        #digita "Verificar se a quantidade de parafusos é suficiente"
-
-        self.fail('Finish the test!')
-
 
        
 #É convidado a inserir um item agora mesmo
