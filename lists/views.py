e2f016357de42724b1499db39ae7b01fe5b2d284
ubuntu-py <you@example.com>
2016-04-21 05:08:46 -0700

    Mudanca

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-#from django.http import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,6 +6,10 @@ from django.shortcuts import render
 #home_page = None
 
 def home_page(request):
-    return render(request, 'home.html')
+    #if request.method == 'POST':
+    #    return HttpResponse(request.POST['item_text'])
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
     #return HttpResponse('<html><title>To-Do lists</title></html>')
     #pass
