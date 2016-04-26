8d8435fa72b7cc48363c9c05fffcf094c18f4753
nandokm <nandokm@gmail.com>
2016-04-26 14:09:29 -0300

    def

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,9 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+#from lists.models import Item
+from lists.models import Item, List
+
 
 class ListViewTest(TestCase):
 
@@ -13,37 +15,49 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
         
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
 
-class ItemModelTest(TestCase):
-    
+#class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
+        
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+        
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
 
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+        
         saved_items= Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
     
 class HomePageTest(TestCase):
    
     def test_root_url_resolves_to_home_page_view(self):
-        found =  resolve('/')
+        found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
@@ -51,11 +65,6 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
-
-    #def test_home_page_only_saves_items_when_necessary(self):
-    #    request = HttpRequest()
-    #    home_page(request)
-    #    self.assertEqual(Item.objects.count(), 0)
 
 class NewListTest(TestCase):
     
@@ -69,14 +78,5 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertRedirects(response,  '/lists/the-only-list-in-the-world/')
-        
-        #self.assertEqual(response.status_code, 302)
-        #self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-        
-        #self.assertIn('A new list item', response.content.decode())
-        #expected_html = render_to_string(
-        #    'home.html',
-        #    {'new_item_text': 'A new list item'}
-        #)
-        #self.assertEqual(response.content.decode(), expected_html)
+
 
--- lists/views.py
+++ lists/views.py
@@ -1,31 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 
 #home_page = None
 
 def home_page(request):
-
     return render(request, 'home.html')
-
-    #if request.method == 'POST':
-        #new_item_text = request.POST['item_text']
-    #    Item.objects.create(text=request.POST['item_text'])
-    #    return redirect('/lists/the-only-list-in-the-world/')
-
-    #items = Item.objects.all()
-
-    #return render(request, 'home.html', {'items':items})
-
-    #return HttpResponse('<html><title>To-Do lists</title></html>')
-    #pass
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items':items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])    
+    list_ = List.objects.create()
+    
+    Item.objects.create(text=request.POST['item_text'], list=list_)    
     return redirect('/lists/the-only-list-in-the-world/')
