569677aadc9fffdd33281fc94fbac2abaa8e7a6a
nandokm <nandokm@gmail.com>
2016-04-26 12:24:34 -0300

    views tests new URL

--- lists/views.py
+++ lists/views.py
@@ -7,14 +7,17 @@ from lists.models import Item
 #home_page = None
 
 def home_page(request):
-    if request.method == 'POST':
+
+    return render(request, 'home.html')
+
+    #if request.method == 'POST':
         #new_item_text = request.POST['item_text']
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
+    #    Item.objects.create(text=request.POST['item_text'])
+    #    return redirect('/lists/the-only-list-in-the-world/')
 
-    items = Item.objects.all()
+    #items = Item.objects.all()
 
-    return render(request, 'home.html', {'items':items})
+    #return render(request, 'home.html', {'items':items})
 
     #return HttpResponse('<html><title>To-Do lists</title></html>')
     #pass
@@ -22,3 +25,7 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items':items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])    
+    return redirect('/lists/the-only-list-in-the-world/')
