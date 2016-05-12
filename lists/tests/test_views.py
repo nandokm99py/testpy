d232cfd446bff55193ae3554cadf41f44697aff0
Nando <nandokm99py@gmail.com>
2016-05-12 13:31:40 -0300

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,7 +4,6 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-#from lists.models import Item
 from lists.models import Item, List
 
 
@@ -63,41 +62,6 @@ class ListViewTest(TestCase):
         list_ = List.objects.create()
         Item.objects.create(text='itemey 1', list=list_)
         Item.objects.create(text='itemey 2', list=list_)      
-#              
-#*        response = self.client.get('/lists/the-only-list-in-the-world/')
-
-#*        self.assertContains(response, 'itemey 1')
-#*        self.assertContains(response, 'itemey 2')
-
-#class ItemModelTest(TestCase):
-class ListAndItemModelsTest(TestCase):
-        
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-        
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-        
-        saved_items= Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
     
 class HomePageTest(TestCase):
    
@@ -124,5 +88,3 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response,  '/lists/%d/' % (new_list.id,))
-
-
