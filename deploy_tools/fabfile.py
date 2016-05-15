42c7b5424cdba584ec544e106de1cfd422a1bc32
Nando <nandokm99py@gmail.com>
2016-05-15 09:41:38 -0300

    fabric1

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -4,42 +4,7 @@ import random
 
 #REPO_URL = 'https://github.com/hjwp/book-example.git'
 #REPO_URL = 'https://github.com/nandokm99py/book-example.git'
-REPO_URL = 'https://github.com/nandokm99py/testepy.git'
-
-
-def deploy():
-    site_folder = '/home/%s/sites/%' % (env.user, env.host)
-    source_folder = site_folder + '/source'
-    _create_directory_structure_if_necessary(site_folder)
-    _get_latest_source(source_folder)
-    _update_settings(source_folder, env.host)
-    _update_virtualenv(source_folder)
-    _update_static_files(source_folder)
-    _update_database(source_folder)
-
-def _create_directory_structure_if_necessary(site_folder):
-    for subfolder in ('database', 'static', 'virtualenv', 'source'):
-        run('mkdir -p %s/%s' % (site_folder, subfolder))
-
-def _get_latest_source(source_folder):
-    pass
-
-def _update_settings(source_folder, site_name):
-    pass
-
-def _update_virtualenv(source_folder):
-    pass
-
-def _update_static_files(source_folder):
-    pass
-
-def _update_database(source_folder):
-    pass
-from fabric.contrib.files import append, exists, sed
-from fabric.api import env, local, run
-import random
-
-REPO_URL = 'https://github.com/hjwp/book-example.git'  #
+REPO_URL = 'https://github.com/nandokm99py/testpy.git'
 
 def deploy():
     site_folder = '/home/%s/sites/%s' % (env.user, env.host)  #
