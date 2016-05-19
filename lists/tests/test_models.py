69a806c50a9dfe6502c68d141381574719e1381e
Nando <nandokm99py@gmail.com>
2016-05-19 18:14:56 -0300

    Implement duplicate item validation at model layer

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -2,11 +2,19 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 from lists.models import Item, List
 
-class ListAndItemModelsTest(TestCase):
+#class ListAndItemModelsTest(TestCase):
+class ItemModelTest(TestCase):
 
     def test_default_text(self):
         item = Item()
-        self.assertEqual(item.text)
+        self.assertEqual(item.text, '')
+
+    def test_item_is_related_to_list(self):
+        list_ = List.objects.create()
+        item = Item()
+        item.list = list_
+        item.save()
+        self.assertIn(item, list_.item_set.all())
         
 #    def test_saving_and_retrieving_items(self):
 #        list_ = List()
@@ -44,6 +52,8 @@ class ListAndItemModelsTest(TestCase):
             item.save()
             item.full_clean()
 
+class ListModelTest(TestCase):
+
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
@@ -54,6 +64,7 @@ class ListAndItemModelsTest(TestCase):
         with self.assertRaises(ValidationError):
             item = Item(list=list_, text='bla')
             item.full_clean()
+            #item.save()
 
     def test_CAN_save_same_item_to_different_lists(self):
         list1 = List.objects.create()
