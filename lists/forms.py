2d6eafb3ab891497627b9471eeb7450633af6edb
Nando <nandokm99py@gmail.com>
2016-05-21 05:21:24 -0300

    after Django's internals - casa

--- lists/forms.py
+++ lists/forms.py
@@ -1,6 +1,7 @@
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 EMPTY_ITEM_ERROR ="You can't have an empty list item"
 
+from django.core.exceptions import ValidationError
 from django import forms
 from lists.models import Item
 
@@ -27,3 +28,12 @@ class ExistingListItemForm(ItemForm):
 
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
