120bb74a730a67da7f7b8e73f36e279ee1b0de06
Nando <nandokm99py@gmail.com>
2016-05-28 08:56:53 -0300

    temp

---
+++ accounts/static/accounts.js
@@ -0,0 +1,22 @@
+var initialize = function (navigator, user, token, urls) {
+    $('#id_login').on('click', function () {
+	    navigator.id.request();
+	});
+
+    navigator.id.watch({
+        loggedInUser: user,
+        onlogin: function (assertion) {
+            $.post(
+                urls.login,
+                { assertion: assertion, csrfmiddlewaretoken: token }
+            );
+        }
+    });
+
+};
+
+window.Superlists = {
+    Accounts: {
+        initialize: initialize
+    }
+};
