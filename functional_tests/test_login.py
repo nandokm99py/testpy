b1da0fc24b155be56ae70d836a2f6f76808e4e7f
Nando <nandokm99py@gmail.com>
2016-06-02 14:10:13 -0300

    placeholder test_my_lists and move login checkers into base

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,3 +1,4 @@
+TEST_EMAIL = 'edith@mockmyid.com'
 from .base import FunctionalTest
 import time
 from selenium.webdriver.support.ui import WebDriverWait
@@ -17,7 +18,7 @@ class LoginTest(FunctionalTest):
         ## Use mockmyid.com for test email
         self.browser.find_element_by_id(
             'authentication_email'
-        ).send_keys('edith@mockmyid.com')
+        ).send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name('button').click()
 
         # The Persona window closes
@@ -44,20 +45,20 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         # Refreshing the page, she sees it's a real session login,
         # not just a one-off for that page
         self.browser.refresh()
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         # Terrified of this new feature, she reflexively clicks "logout"
         self.browser.find_element_by_id('id_logout').click()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
 
         # The "logged out" status also persists after a refresh
         self.browser.refresh()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
 
     def switch_to_new_window(self, text_in_title):
         retries = 60
